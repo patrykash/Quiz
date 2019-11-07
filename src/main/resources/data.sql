36b866a2e59fcea8ea688ccfbadf88ef343358ee
patrykash <patryksieczka4@gmail.com>
2019-11-07 15:41:17 +0100

    add data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,21 @@
+INSERT INTO players VALUES
+    (1,'Patryk',135),
+    (2,'Arek',10),
+    (3,'Anna',70),
+    (4,'Agnieszka',270),
+    (5,'Rafał',45),
+    (6,'Bartek',0),
+    (7,'Adam',0),
+    (8,'Monika',175);
+	
+INSERT INTO questions VALUES
+    (1,'1691','1791','1719','1619','W którym roku została uchwalona konstytucja 3 maja?','B'),
+    (2,'1918','1914','1921','1945','W którym roku Polska odzyskała niepodległość?','A'),
+    (3,'996','969','966','956','W którym roku miał miejsce Chrzest Polski?','C'),
+    (4,'Manchester United','FC Barcelona','Juventus FC','Real Madryt','Która drużyna jako pierwszą zdołała obronić tytuł w piłkarskiej Lidze Mistrzów:','D'),
+    (5,'USA','ZSRR','Hiszpanii','Chorwacji','Reprezentacja w koszykówce jakiego kraju została nazywana "dream teamem"?','A'),
+    (6,'Lewis Hamilton','Michael Schumacher','Niki Lauda','Ayrton Senna','Najwięcej tytułów mistrza świata Formuły 1 posiada:','B'),
+    (7,'Golden State Warriors','Los Angeles Lakers','Boston Celtics','Chicago Bulls','Drużyna, która najwięcej razy zdobyła mistrzostwo ligi NBA to:','C'),
+    (8,'Europie','Australii','Ameryce Północnej','Azji','Gdzie znajduję się Góra Kościuszki?','B'),
+    (9,'Stany Zjednoczone','Argentynę','Kolumbię','Kanadę','Pasmo Gór Skalistych ciągnie się przez:','A'),
+    (10,'Niemcami','Czechami','Ukrainą','Rosją','Polska ma najdłuższą granicę z:','B');
